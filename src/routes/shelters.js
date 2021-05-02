a55cc675e8f7d520eb357e392bc53ccd086ea512
elisasoto <71519081+elisasoto@users.noreply.github.com>
2021-05-02 20:14:51 +0200

    endpoint to edit shelter profile

--- src/routes/shelters.js
+++ src/routes/shelters.js
@@ -1,3 +1,37 @@
 const router = require('express').Router();
+const omitBy = require('lodash/omitBy');
+
+const ShelterModel = require('../../models/Shelter');
+
+const { isAuthenticated } = require('../middlewares/authentication');
+
+router.put('/edit', [isAuthenticated], async (req, res, next) => {
+  try {
+    const Shelter = await ShelterModel.findById(req.user);
+
+    if (!Shelter) {
+      const error = new Error('Shelter not found');
+      error.code = 404;
+      throw error;
+    }
+
+    const filteredShelter = omitBy(req.body, (value, _) => !value);
+
+    const result = await ShelterModel.findByIdAndUpdate(
+      req.user,
+      filteredShelter,
+      {
+        new: true
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
