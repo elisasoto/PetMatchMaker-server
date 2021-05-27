5eda1edc531e659732e1ab69af3b1021be1a2bb4
elisasoto <71519081+elisasoto@users.noreply.github.com>
2021-05-27 19:02:30 +0200

    debloyed server with DB URI

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const DB_URI = 'mongodb://localhost:27017/petMatchMaker';
+// const DB_URI = 'mongodb://localhost:27017/petMatchMaker';
+const DB_URI = process.env.DB_URI;
 
 mongoose
   .connect(DB_URI, {
